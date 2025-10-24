498656fe6bced8d702c4c58d334c8c028a900b5c
Seraphim <Nihaz.444.s.d@mail.ru>
2025-10-24 22:32:45 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from database import delete_tables, create_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tables()
+    print("База создана")
+    yield
+    print("База закрыта",)
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
